c920b47e88454ef3e29b8efe5f8dec8c70d40510
takemo101 <takemo101@gmail.com>
2023-02-22 00:23:26 +0900

    ちょっとデザイン入れる

--- setting/helper.php
+++ setting/helper.php
@@ -3,6 +3,7 @@
 use Takemo101\Egg\Kernel\Application;
 use Takemo101\Egg\Support\StaticContainer;
 use Takemo101\Egg\Http\Filter\CsrfFilter;
+use Takemo101\Egg\Support\Filesystem\URLHelper;
 
 if (!function_exists('csrf_token')) {
     /**
@@ -19,5 +20,36 @@ if (!function_exists('csrf_token')) {
         $filter = $app->container->make(CsrfFilter::class);
 
         return $filter->token();
+    }
+}
+
+if (!function_exists('url')) {
+    /**
+     * url取得
+     *
+     * @param string|null $path
+     * @return string
+     */
+    function url(?string $path = null): string
+    {
+        $helper = new URLHelper();
+
+        return $helper->join(
+            config('app.url', '/'),
+            $path,
+        );
+    }
+}
+
+if (!function_exists('asset')) {
+    /**
+     * assetパス取得
+     *
+     * @param string|null $path
+     * @return string
+     */
+    function asset(?string $path = null): string
+    {
+        return url($path);
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './resource/**/*.latte.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
